054c306051b764ffaafdcf549adc3177d34a00b3
Prakhar Srivastava <sriprakhar2@gmail.com>
2024-07-03 15:23:13 +0530

    chore: svgs, constants, fonts added

---
+++ src/constants.ts
@@ -0,0 +1,39 @@
+export const INPUT_TYPE = {
+  TEXT: 'text',
+  PASSWORD: 'password',
+};
+
+export const LOGIN_MODAL_TEXT = {
+  headerText: 'WELCOME BACK',
+  descriptionText: 'Log into your account',
+  footerStatementText: 'Not registered yet?',
+  redirectionText: 'Register',
+  email: {
+    title: 'Email or Username',
+    placeholder: 'Enter your email or username',
+  },
+  password: {
+    title: 'Password',
+    placeholder: 'Enter your password',
+  },
+  buttonText: 'Login Now',
+};
+export const REGISTER_MODAL_TEXT = {
+  headerText: 'SIGN UP',
+  descriptionText: 'Create an account to continue',
+  footerStatementText: 'Already have an account?',
+  redirectionText: 'Login',
+  email: {
+    title: 'Email',
+    placeholder: 'Enter your email',
+  },
+  username: {
+    title: 'Username',
+    placeholder: 'Choose a preffered username',
+  },
+  password: {
+    title: 'Password',
+    placeholder: 'Choose a strong password',
+  },
+  buttonText: 'Continue',
+};
