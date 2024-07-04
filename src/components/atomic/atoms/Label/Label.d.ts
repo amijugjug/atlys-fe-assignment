49b740948da6da62d6571b8458d6c7778a905f94
Prakhar Srivastava <sriprakhar2@gmail.com>
2024-07-04 13:44:20 +0530

    chore : label interface updated more dynamic nature

---
+++ src/components/atomic/atoms/Label/Label.d.ts
@@ -0,0 +1,10 @@
+export interface ILabel {
+  title: string;
+  onClick?: () => void;
+  size: string;
+  weight?: number;
+  color?: string;
+  lineHeight?: string;
+  textAlign?: 'center' | 'left' | 'right';
+  cursor?: 'pointer' | 'default';
+}
--- src/components/atomic/atoms/Label/index.tsx
+++ src/components/atomic/atoms/Label/index.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
+import s from './Label.module.scss';
+import { ILabel } from './Label';
 
 const Label = ({
-  className,
   title,
-  onClick,
-}: {
-  className: any;
-  title: string;
-  onClick?: () => void;
-}) => {
+  onClick = () => {},
+  size = '16px',
+  weight = 500,
+  color = '#FFFFFF',
+  lineHeight = '16.94px',
+  textAlign = 'left',
+  cursor = 'default',
+}: ILabel) => {
+  const style = {
+    textAlign: textAlign,
+    fontSize: size,
+    fontWeight: weight,
+    color: color,
+    lineHeight: lineHeight,
+    cursor: cursor,
+  };
+
   return (
-    <label className={className} onClick={onClick}>
+    <label className={s.label} style={style} onClick={onClick}>
       {title}
     </label>
   );
