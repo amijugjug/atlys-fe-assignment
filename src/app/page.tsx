d36695e16b04404a7740457112890d65dbb46608
Prakhar Srivastava <sriprakhar2@gmail.com>
2024-07-04 13:47:27 +0530

    chore : directory for create-box and post-container initialized with files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 'use client';
+import Avatar from '@/components/atomic/atoms/Avatar';
+import UserInfoStrip from '@/components/atomic/molecules/UserInfoStrip';
 import Portal from '@/components/atomic/organisms/Portal';
 import LoginRegisterModal from '@/components/atomic/templates/LoginRegisterModal';
 import React, { useEffect, useState } from 'react';
@@ -11,9 +13,17 @@ const Home = () => {
       localStorage.setItem('DB', JSON.stringify({}));
     }
   }, []);
+  const profileItems = {
+    name: 'Prakhar Srivastava',
+    createdAt: 1720067648495,
+    isEdited: true,
+    profileImageUrl:
+      'https://images.firstpost.com/uploads/2024/01/Kareena.jpg?im=FitAndFill=(596,336)',
+  };
   return (
     <>
       <div>Home</div>
+      <UserInfoStrip profileDetails={profileItems} />
       <button onClick={toggleModal}>Toggle Modal</button>
       {modal ? (
         <Portal toggleModal={toggleModal}>
